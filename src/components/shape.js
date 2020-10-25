b0acd1c56f839e1e64d3c608cb6c77937d0d1039
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-10-25 23:41:48 +0530

    Changes

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,11 @@
 // declare class
+class Shape {
+  constructor() {
+    this.color = "red";
+  }
+  drawShape() {}
+  calculateArea() {}
+}
 
 // export class using module.exports
+module.exports = Shape;
